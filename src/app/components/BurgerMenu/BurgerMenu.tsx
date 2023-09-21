6b7ca320d46f64795a0d027ca902d4188570a034
achiewe <mamukashviliachi706@gmail.com>
2023-09-21 19:57:28 +0400

    create navbar for burgerMenu

--- src/app/components/BurgerMenu/BurgerMenu.tsx
+++ src/app/components/BurgerMenu/BurgerMenu.tsx
@@ -1,4 +1,5 @@
 import styles from "./BurgerMenu.module.scss";
+import Navbar from "../Navbar/Navbar";
 
 const BurgerMenu = (): JSX.Element => {
   return (
@@ -20,10 +21,10 @@ const BurgerMenu = (): JSX.Element => {
         />
 
         <g id="SVGRepo_iconCarrier">
-          {" "}
           <path d="M202.82861,197.17188a3.99991,3.99991,0,1,1-5.65722,5.65624L128,133.65723,58.82861,202.82812a3.99991,3.99991,0,0,1-5.65722-5.65624L122.343,128,53.17139,58.82812a3.99991,3.99991,0,0,1,5.65722-5.65624L128,122.34277l69.17139-69.17089a3.99991,3.99991,0,0,1,5.65722,5.65624L133.657,128Z" />{" "}
         </g>
-      </svg>{" "}
+      </svg>
+      <Navbar />
     </div>
   );
 };
---
+++ src/app/components/Navbar/Navbar.tsx
@@ -0,0 +1,5 @@
+const Navbar = (): JSX.Element => {
+  return <ul className=""> </ul>;
+};
+
+export default Navbar;
