4ddd436b5561631d9abda6f78b7b488db757ecb9
achiewe <mamukashviliachi706@gmail.com>
2023-09-21 15:31:49 +0400

    create scss file for main home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Header from "./components/Header/Header";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1> hello world</h1>
+    <main>
+      <Header />
     </main>
   );
 }
