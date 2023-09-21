4c530fcd4bfaf514d3abd689cacddb1dc5b47539
achiewe <mamukashviliachi706@gmail.com>
2023-09-21 15:48:32 +0400

    create header module scss file for styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import Header from "./components/Header/Header";
+import styles from "./page.module.scss";
 
 export default function Home() {
-  return (
-    <main>
-      <Header />
-    </main>
-  );
+  return <main></main>;
 }
