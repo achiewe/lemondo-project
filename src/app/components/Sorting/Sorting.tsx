95d9c4b2c832ff89f3829e33cc3ed33012a7dabb
achiewe <mamukashviliachi706@gmail.com>
2023-09-22 13:40:38 +0400

    create sorting divs and style it

--- src/app/components/Sorting/Sorting.tsx
+++ src/app/components/Sorting/Sorting.tsx
@@ -5,14 +5,16 @@ import Image from "next/image";
 
 const Sorting = (): JSX.Element => {
   return (
-    <div className={styles.MainSorting}>
-      <div className={styles.filterSorting}>
-        <h4 className={styles.sortingTitle}>სორტირება </h4>
-        <Image src={sortSvg} alt="sort image" />
-      </div>
-      <div className={styles.filterSorting}>
-        <h4 className={styles.sortingTitle}>სორტირება </h4>
-        <Image src={arrowSvg} alt="sort image" />
+    <div className={styles.mainContainer}>
+      <div className={styles.MainSorting}>
+        <div className={styles.filterSorting}>
+          <h4 className={styles.sortingTitle}>სორტირება </h4>
+          <Image src={sortSvg} alt="sort image" />
+        </div>
+        <div className={styles.filterSorting}>
+          <h4 className={styles.sortingTitle}>სორტირება </h4>
+          <Image src={arrowSvg} alt="sort image" />
+        </div>
       </div>
     </div>
   );
